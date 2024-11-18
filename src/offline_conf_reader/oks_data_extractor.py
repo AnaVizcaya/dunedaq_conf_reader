52e92c1a5003518131eb8b5b51e0416e15125406
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-11-18 10:36:30 +0100

    make create a simple get function to grab classes or objectnames

--- src/offline_conf_reader/oks_data_extractor.py
+++ src/offline_conf_reader/oks_data_extractor.py
@@ -3,10 +3,48 @@ from pathlib import Path
 from defusedxml.ElementTree import parse
 
 
-def check_for_includes(root):
+def check_for_data_includes(root):
     for child in root:
-        if child.tag == 'include':
+        if child.tag != 'include':
+            continue
+        for include_file in child:
+            if include_file.tag != 'file':
+                continue
+            if include_file.attrib['path'].endswith('.schema.xml'):
+                continue
             return True
+
+    return False
+
+def find_session(root, session_name):
+    for child in root:
+        if child.tag == 'obj' and child.attrib['class'] == "Session" and child.attrib['id'] == session_name:
+            return child
+
+
+def get(root, start_obj, object_name=None, class_name=None):
+
+    if object_name is None and class_name is None:
+        raise ValueError('Either \'object_name\' or \'class_name\' must be provided')
+
+    for child in start_obj:
+        if class_name is not None and child.attrib.get('class') is not None and child.attrib.get('class') != class_name:
+            continue
+
+        if object_name is not None and child.attrib.get('name') is not None and child.attrib.get('name') != object_name:
+            continue
+
+        if child.tag == 'attr':
+            return child
+
+        if child.tag == 'rel':
+            for obj in root.findall('obj'):
+                if obj.attrib['id'] != child.attrib['id']:
+                    continue
+                return obj
+    raise ValueError(f'Could not find object with name \'{object_name}\' or class \'{class_name}\' in {start_obj.attrib["id"]}, which is composed of: {[s.attrib["name"] for s in start_obj]}')
+
+
 
 @dataclass
 class OKSDataExtractor:
@@ -36,8 +74,18 @@ class OKSDataExtractor:
         tree = parse(self.oks_file_path)
         root = tree.getroot()
 
-        if check_for_includes(root):
+        if check_for_data_includes(root):
             raise RuntimeError('Include files are not supported, the configuration was not consolidated!')
+
+        session = find_session(root, self.session_name)
+        print(session.attrib)
+        for session_child in session:
+            print(session_child.tag, session_child.attrib)
+
+        print(get(root, session, object_name='data_rate_slowdown_factor').attrib)
+
+        print(get(root, session, object_name='segment').attrib)
+
 
 
     def get_variables(self) -> list[Field]:
