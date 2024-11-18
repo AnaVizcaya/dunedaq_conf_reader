bc4cb43507df094f6222ad31009fb91bda54e629
Pierre Lasorak <pierre.lasorak@gmail.com>
2024-11-18 10:35:28 +0100

    keyword arguments

--- src/offline_conf_reader/test/test_extract_data.py
+++ src/offline_conf_reader/test/test_extract_data.py
@@ -6,7 +6,7 @@ from offline_conf_reader.oks_data_extractor import OKSDataExtractor
 def test_can_extract_data(ehn1_daqconfig_sessions, variables_extracted):
     for session_name, file_path in ehn1_daqconfig_sessions().items():
         print(f'Processing \'{session_name}\' in {file_path}')
-        ode = OKSDataExtractor(session_name, oks_file_path=file_path)
+        ode = OKSDataExtractor(oks_file_path=file_path, session_name=session_name)
 
         for variable_name, variable_type in variables_extracted.items():
             print(f'Checking variable \'{variable_name}\'')
@@ -18,5 +18,5 @@ def test_does_not_accept_include(ehn1_daqconfig_sessions, variables_extracted):
     for session_name, file_path in ehn1_daqconfig_sessions(False).items():
         print(f'Processing \'{session_name}\' in {file_path}')
         with pytest.raises(Exception):
-            ode = OKSDataExtractor(session_name, oks_file_path=file_path)
+            ode = OKSDataExtractor(oks_file_path=file_path, session_name=session_name)
 
