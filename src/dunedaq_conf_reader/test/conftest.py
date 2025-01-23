31853afd39c75180851be11609c3942a016549fb
Pierre Lasorak <pierre.lasorak@gmail.com>
2025-01-23 18:53:12 +0100

    correct URL

--- src/dunedaq_conf_reader/test/conftest.py
+++ src/dunedaq_conf_reader/test/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--branch-name", action="store", default="develop")
-    parser.addoption("--repository", action="store", default="base")
+    parser.addoption("--repository", action="store", default="ssh://git@gitlab.cern.ch:7999/dune-daq/online/ehn1-daqconfigs.git")
 
 @pytest.fixture
 def branch_name(request):
@@ -10,8 +10,4 @@ def branch_name(request):
 
 @pytest.fixture
 def repository(request):
-
-    if request.config.option.repository not in ["base", "operation"]:
-        raise ValueError("Invalid repo name, should be either 'base' or 'operation'")
-
     return request.config.option.repository
